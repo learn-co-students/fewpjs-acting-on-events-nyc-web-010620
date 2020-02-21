d4956bc243c4bd2c05fda0ea57be4e77efba1722
github username <github email address>
2020-02-20 23:31:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.querySelector('#dodger')
+document.addEventListener('keydown', function (event) {
+    switch(true) {
+        case event.key === 'ArrowLeft':
+            moveDodgerLeft()
+            break
+        case event.key === 'ArrowRight':
+            moveDodgerRight()
+            break
+    }
+})
+
+let currentPos = parseInt(dodger.style.left)
+
+const moveDodgerLeft = () => {
+   currentPos -= 1
+   dodger.style.left = `${currentPos}px`
+}
+
+const moveDodgerRight = () => {
+   currentPos += 1
+   dodger.style.left = `${currentPos}px`
+}
